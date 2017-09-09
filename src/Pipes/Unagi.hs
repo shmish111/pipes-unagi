b60c921cb786d989afdc76473851044ef8aea599
David Smith <shmish111@gmail.com>
2017-09-09 18:11:15 +0100

    now combine works but replicateP has some problems

--- src/Pipes/Unagi.hs
+++ src/Pipes/Unagi.hs
@@ -1,70 +1,123 @@
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module Pipes.Unagi where
 
-import Pipes (Producer, Consumer, (>->), yield, await, runEffect)
-import Control.Concurrent.Chan.Unagi (InChan, OutChan, newChan, readChan, writeChan)
-import Control.Monad (forever, void)
-import Control.Concurrent.MonadIO (MonadIO, HasFork, liftIO, fork, threadDelay, forkIO)
-import Control.Monad.Trans.Class (lift)
-import qualified Pipes.Prelude as P
+import           Control.Concurrent.Async.Lifted (Async, async, poll, waitBoth)
+import           Control.Concurrent.Chan.Unagi   (InChan, OutChan, newChan,
+                                                  readChan, writeChan)
+import           Control.Concurrent.MonadIO      (HasFork, MonadIO, fork,
+                                                  forkIO, liftIO, threadDelay)
+import           Control.Exception.Base          (SomeException)
+import           Control.Monad                   (forever, void)
+import           Control.Monad.Trans.Class       (lift)
+import           Control.Monad.Trans.Control     (MonadBaseControl, StM)
+import           Pipes                           (Consumer, Producer, await,
+                                                  runEffect, yield, (>->))
+import qualified Pipes.Prelude                   as P
 
-producer :: (MonadIO m) => OutChan a -> Producer a m r
-producer outChan = forever $ do
-  msg <- liftIO $ readChan outChan
-  yield msg
-  -- TODO: catch BlockedIndefinitelyOnMVar and close producer properly
-  -- If I can use async, I can instead poll a1 and a2 and if they are both complete
-  -- I will return a tuple of their values. This should then close as soon as p1 and
-  -- p2 are both closed and no BlockedIndefinitelyOnMVar will be thrown
-
-combine :: (MonadIO m, HasFork m) => Producer a m r -> Producer a m r -> Producer a m r
+combine ::
+     (MonadIO m, HasFork m, MonadBaseControl IO m)
+  => Producer a m r
+  -> Producer a m r
+  -> Producer a m (Either SomeException r, Either SomeException r)
 combine p1 p2 = do
-  (inChan, outChan) <- liftIO $ newChan
-  t1 <- lift . fork . void . runEffect $ p1 >-> (consumer inChan)
-  t2 <- lift . fork . void . runEffect $ p2 >-> (consumer inChan)
-  producer outChan
+  (inChan, outChan) <- liftIO newChan
+  a1 <- lift . async . runEffect $ p1 >-> consumer inChan
+  a2 <- lift . async . runEffect $ p2 >-> consumer inChan
+  producer a1 a2 outChan
   where
     consumer :: (MonadIO m) => InChan a -> Consumer a m r
-    consumer inChan = forever $ do
-      msg <- await
-      liftIO $ writeChan inChan msg
+    consumer inChan =
+      forever $ do
+        msg <- await
+        liftIO $ writeChan inChan msg
+    producer ::
+         (MonadIO m, MonadBaseControl IO m)
+      => Async (StM m r)
+      -> Async (StM m r)
+      -> OutChan a
+      -> Producer a m (Either SomeException r, Either SomeException r)
+    producer a1 a2 outChan = do
+      mv1 <- lift $ poll a1
+      mv2 <- lift $ poll a2
+      case (mv1, mv2) of
+        (Just v1, Just v2) -> do
+          msg <- liftIO $ readChan outChan
+          yield msg
+          pure (v1, v2)
+        _ -> do
+          msg <- liftIO $ readChan outChan
+          yield msg
+          producer a1 a2 outChan
 
 -- TODO: combineMany :: (MonadIO m, HasFork m) => [Producer a m r] -> Producer a m [r]
-
 testCombine :: IO ()
 testCombine = do
-  runEffect $ (combine p1 p2) >-> P.stdoutLn
+  runEffect $ combine p1 p2 >-> pstdoutLn
+  pure ()
   where
+    pstdoutLn =
+      forever $ do
+        a <- await
+        liftIO $ putStrLn a
+        pure ((), ())
     produce c = do
-      liftIO $ threadDelay 10000000
+      liftIO $ threadDelay 1000000
       pure c
     p1 = P.replicateM 2 $ produce "a"
     p2 = P.replicateM 2 $ produce "b"
 
-replicateP :: (MonadIO m, MonadIO n, HasFork n) => Producer a n r -> n [Producer a m r]
+replicateP ::
+     (MonadIO n, MonadBaseControl IO n)
+  => Producer a n r
+  -> n [Producer a n (Either SomeException r)]
 replicateP p = do
-  (inChan1, outChan1) <- liftIO $ newChan
-  (inChan2, outChan2) <- liftIO $ newChan
-  t <- fork . void . runEffect $ p >-> (consumer inChan1 inChan2)
-  pure $ [(producer outChan1), (producer outChan2)]
+  (inChan1, outChan1) <- liftIO newChan
+  (inChan2, outChan2) <- liftIO newChan
+  a <- async . runEffect $ p >-> consumer inChan1 inChan2
+  pure [producer a outChan1, producer a outChan2]
   where
     consumer :: (MonadIO m) => InChan a -> InChan a -> Consumer a m r
-    consumer i1 i2 = forever $ do
-      m <- await
-      liftIO $ writeChan i1 m
-      liftIO $ writeChan i2 m
+    consumer i1 i2 =
+      forever $ do
+        m <- await
+        liftIO $ writeChan i1 m
+        liftIO $ writeChan i2 m
+    producer ::
+         (MonadIO m, MonadBaseControl IO m)
+      => Async (StM m r)
+      -> OutChan a
+      -> Producer a m (Either SomeException r)
+    producer a outChan = do
+      mv <- lift $ poll a
+      case mv of
+        Nothing -> do
+          liftIO $ putStrLn "nowt"
+          msg <- liftIO $ readChan outChan
+          yield msg
+          producer a outChan
+        Just v -> do
+          liftIO $ putStrLn "just"
+          msg <- liftIO $ readChan outChan
+          yield msg
+          pure v
 
 testReplicate :: IO ()
 testReplicate = do
   [p1, p2] <- replicateP p
-  forkIO . runEffect $ p1 >-> P.stdoutLn
-  forkIO . runEffect $ p2 >-> P.stdoutLn
-  threadDelay 1000
+  a1 <- async . void . runEffect $ p1 >-> consumer
+  a2 <- async . void . runEffect $ p2 >-> consumer
+  waitBoth a1 a2
   pure ()
-  -- TODO: should wait for async of p1 and p2 to finish
   where
     produce c = do
-      --liftIO $ threadDelay 10000000
+      liftIO $ threadDelay 1000000
       pure c
     p = P.replicateM 2 $ produce "a"
-
+    consumer =
+      forever $ do
+        esv <- await
+        liftIO $ print esv
+-- TODO: this test isn't working properly
 -- TODO: write tests to check performance and compare to pipes-concurrency
